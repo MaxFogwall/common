630db1f59dc239f109de438bff408f9b3a4509ed
MaxFogwall <48988185+MaxFogwall@users.noreply.github.com>
2024-07-12 17:59:13 +0200

    Fix faulty regex

--- code/github.go
+++ code/github.go
@@ -273,7 +273,7 @@ func GetCurrentRepository() (string, error) {
 	log.Printf("`repoUrl`: %s", repoUrl)
 
 	// E.g. "https://github.com/workflow-sync-poc/common.git" -> "workflow-sync-poc/common"
-	repoFromUrlPattern := regexp.MustCompile(`https://github.com/(.+?)\.git`)
+	repoFromUrlPattern := regexp.MustCompile(`https:\/\/github\.com\/(.+?)\.git`)
 	repoFromUrlMatch := repoFromUrlPattern.FindString(repoUrl)
 	repo := string(repoFromUrlMatch)
 
